4e2588b0f1b57f1e0c3bdf8990bb057f07580ec1
magopd <aryperdomo123456789@gmail.com>
2026-07-23 07:11:20 -0300

    feat(save): accept rate_limit_per_minute

--- proxy-mago-base/public/save.php
+++ proxy-mago-base/public/save.php
@@ -15,13 +15,14 @@ csrf_verify();
 $adminUser = trim((string) ($_POST['admin_user'] ?? ''));
 $adminPass = (string) ($_POST['admin_pass'] ?? '');
 $panelDomain = trim((string) ($_POST['panel_domain'] ?? ''));
-$originHost = trim((string) ($_POST['origin_host'] ?? ''));
+$originHost = trim((string) ($_POST['origin_host'] ?? '127.0.0.1'));
 $originPort = (int) ($_POST['origin_port'] ?? 80);
 $allowedUserAgent = trim((string) ($_POST['allowed_user_agent'] ?? ''));
-$tokenTtl = (int) ($_POST['token_ttl'] ?? 3600);
+$tokenTtl = (int) ($_POST['token_ttl'] ?? Config::get('token_ttl'));
+$rateLimit = (int) ($_POST['rate_limit_per_minute'] ?? Config::get('rate_limit_per_minute'));
 $appSecret = trim((string) ($_POST['app_secret'] ?? ''));
 
-if ($adminUser === '' || $originHost === '' || $originPort < 1) {
+if ($adminUser === '') {
     http_response_code(422);
     exit('Dados inválidos.');
 }
@@ -34,8 +35,9 @@ if ($adminPass !== '') {
 SettingsRepository::set('panel_domain', $panelDomain);
 SettingsRepository::set('origin_host', $originHost);
 SettingsRepository::set('origin_port', $originPort);
-SettingsRepository::set('allowed_user_agent', $allowedUserAgent !== '' ? $allowedUserAgent : Config::get('allowed_user_agent'));
+SettingsRepository::set('allowed_user_agent', $allowedUserAgent);
 SettingsRepository::set('token_ttl', max(60, $tokenTtl));
+SettingsRepository::set('rate_limit_per_minute', max(0, $rateLimit));
 
 if ($appSecret === '') {
     $appSecret = bin2hex(random_bytes(32));
